9651036d30ced67c30961f05d0ec3819cd3b3ea6
pavanvpatil <pavanvpatil01@gmail.com>
2023-03-31 02:09:35 +0530

    The manager periodically, peer informs when it go

--- 200030041_manager.py
+++ 200030041_manager.py
@@ -29,12 +29,16 @@ def check_peer_username(peer_username):
             return True
     return False
 
+
 def new_peer_joined(peer_socket, peer_address, peer_username):
     global active_peers
 
-    while(check_peer_username(peer_username)):
+    while (check_peer_username(peer_username)):
         peer_socket.send('Error in name'.encode())
-        peer_username = peer_socket.recv(1024).decode().split(';')[1]
+        peer_username = peer_socket.recv(1024).decode()
+        peer_username = peer_username.split(';')[1]
+
+    peer_socket.send('Name accepted'.encode())
 
     active_peers.append((peer_socket, peer_address, peer_username))
 
@@ -53,16 +57,51 @@ def new_peer_joined(peer_socket, peer_address, peer_username):
             continue
 
 
+def peer_leave(peer_socket, peer_address):
+    global active_peers
+
+    for peer in active_peers:
+        if peer[1] == peer_address:
+            active_peers.remove(peer)
+            print("Peer left the network: ", peer_address)
+            updated_peer_list = json.dumps(
+                [(peer[1], peer[2]) for peer in active_peers])
+            for peer in active_peers:
+                try:
+                    peer[0].send('update_peer_list'.encode())
+                    time.sleep(0.1)
+                    peer[0].send(updated_peer_list.encode())
+                    print("sent updated list to peer: ", (peer[1], peer[2]))
+                except (socket.error):
+                    print("Peer is no longer active: ", (peer[1], peer[2]))
+                    continue
+            break
+
+    peer_socket.close()
+
+
+def start_peer_listen(peer_socket, peer_address):
+    while True:
+        message = peer_socket.recv(1024).decode()
+        print("message: ", message)
+
+        if (message.split(';')[0] == 'Hello'):
+            new_peer_joined(peer_socket, peer_address, message.split(';')[1])
+            continue
+
+        if (message == 'exit'):
+            print("peer left the network", peer_address)
+            peer_socket.send('exit'.encode())
+            peer_leave(peer_socket, peer_address)
+            break
+
+        if message == "":
+            print("peer left the network", peer_address)
+            peer_leave(peer_socket, peer_address)
+            break
+
+
 while True:
     (peer_socket, peer_address) = Manager_socket.accept()
-
-    message = peer_socket.recv(1024).decode()
-
-    print("message: ", message)
-
-    if (message.split(';')[0] == 'Hello'):
-        thread_A = Thread(target=new_peer_joined,
-                          args=(peer_socket, peer_address, message.split(';')[1]))
-        thread_A.start()
-        thread_A.join()
-        continue
+    thread = Thread(target=start_peer_listen, args=(peer_socket, peer_address))
+    thread.start()
--- 200030041_peer.py
+++ 200030041_peer.py
@@ -3,6 +3,8 @@ import socket
 import time
 from threading import Thread
 import json
+import sys
+from copy import deepcopy
 
 peer_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -24,41 +26,87 @@ peer_address = ('localhost', port)
 
 
 name = input("Enter your name: ")
+accepted = False
+
+
+def peer_features(peer_socket, manager_address):
+    global name
+    while True:
+        print("Username: ", name)
+        print("\nChoose an option: ")
+        print("1. Ask for file")
+        print("2. Exit")
+        choice = int(input("Enter your choice: "))
+
+        if choice == 1:
+            file_name = input("Enter file name: ")
+            # peer_socket.send(('ask_file;' + file_name).encode())
+            continue
+        elif choice == 2:
+            peer_socket.send('exit'.encode())
+            sys.exit(0)
+        else:
+            print("/---------Invalid choice---------/\n\n\n")
+            continue
 
 
 def peer_client(peer_socket, manager_address):
     global name
+    global accepted
     global active_peer_list
     peer_socket.connect(manager_address)
+
     peer_socket.send(('Hello;' + name).encode())
 
     while True:
         message = peer_socket.recv(1024).decode()
 
-        print("message: ", message)
+        # print("message: ", message)
 
         if message == 'update_peer_list':
             message = peer_socket.recv(1024).decode()
             temp_active_peer_list = json.loads(message)
             active_peer_list.clear()
             active_peer_list = temp_active_peer_list.copy()
-            print("updated_peer_list: ", active_peer_list)
+            # print("updated_peer_list: ", active_peer_list)
             continue
 
         if message == 'Error in name':
-            print("User with same name already exists")
+            # print("User with same name already exists")
             name = input("Re-Enter your name: ")
             peer_socket.send(('Hello;' + name).encode())
-            time.sleep(0.1)
             continue
+
+        if message == 'Name accepted':
+            accepted = True
+            continue
+
+        if message == 'exit':
+            print("Exiting............")
+            sys.exit(0)
+
 
 def peer_server(peer_socket):
     peer_socket.listen(1)
-    print('Peer is listening for incoming connections................\n')
+    # print('Peer is listening for incoming connections................\n')
     while True:
         pass
 
 
+# temp_peer_socket1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 thread_A = Thread(target=peer_client, args=(peer_socket, manager_address))
 thread_A.start()
+
+# thread_B = Thread(target=peer_server, args=(peer_socket,))
+# thread_B.start()
+
+# temp_peer_socket2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+while not accepted:
+    continue
+
+thread_C = Thread(target=peer_features, args=(peer_socket, manager_address))
+thread_C.start()
+
 thread_A.join()
+thread_C.join()
